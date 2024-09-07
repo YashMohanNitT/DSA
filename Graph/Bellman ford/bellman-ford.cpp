73ff626670588b41b36a24137d72121f3cb69700
Yash Mohan <yashmohan.nitt@gmail.com>
2024-09-07 15:31:20 +0530

    bellman ford explaination

---
+++ Graph/Bellman ford/bellman-ford.cpp
@@ -0,0 +1,143 @@
+/*
+	- https://visualgo.net/en/sssp
+	
+
+	Explaination 
+	Everywhere above we considered that there is no negative cycle in the graph (precisely, we are interested in a negative cycle that is reachable from the starting vertex v, and, for an unreachable cycles nothing in the above algorithm changes). 
+	In the presence of a negative cycle(s), there are further complications associated with the fact that distances to all vertices in this cycle, as well as the distances to the vertices reachable from this cycle is not defined 
+	— they should be equal to minus infinity.
+
+	It is easy to see that the Bellman-Ford algorithm can endlessly do the relaxation among all vertices of this cycle and the vertices reachable from it. 
+	Therefore, if you do not limit the number of phases to n - 1, the algorithm will run indefinitely, constantly improving the distance from these vertices.
+
+	Hence we obtain the criterion for presence of a cycle of negative weights reachable for source vertex v: 
+		after N-1 phase if we run algorithm for one more phase, and it performs at least one more relaxation, then the graph contains a negative weight cycle that is reachable from v; 
+		otherwise, such a cycle does not exist.
+
+	Moreover, if such a cycle is found, the Bellman-Ford algorithm can be modified so that it retrieves this cycle as a sequence of vertices contained in it.
+	For this, it is sufficient to remember the last vertex x for which there was a relaxation in nth phase. This vertex will either lie in a negative weight cycle, or is reachable from it. 
+	To get the vertices that are guaranteed to lie in a negative cycle, starting from the vertex x, pass through to the predecessors n times. 
+	Hence we will get the vertex y, namely the vertex in the cycle earliest reachable from source. 
+	We have to go from this vertex, through the predecessors, until we get back to the same vertex y (and it will happen, because relaxation in a negative weight cycle occur in a circular manner).
+
+
+*/
+
+
+
+#include <bits/stdc++.h>
+ 
+#define endl            "\n"
+#define all(x)           x.begin(), x.end()
+#define yash            ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define w()             int t; cin>>t; while(t--)
+#define rep(i,a,b)      for(int i=a;i<b;++i)
+#define vpii            vector<pair<int, int>>
+#define vi              vector<int>
+#define vvi             vector<vector<int>>
+#define read(v,n)       rep(i,0,n) cin>>v[i];
+#define int             long long
+#define ull             unsigned long long
+#define out(v,n)        rep(i,0,n) {cout<<v[i]<<" ";}cout<<endl;
+#define pq              priority_queue
+#define mod             1000000007
+ 
+using namespace std;
+vector<string> split(const string& s, char c) {
+	vector<string> v; stringstream ss(s); string x;
+	while (getline(ss, x, c)) v.emplace_back(x); return move(v);
+}
+template<typename T, typename... Args>
+inline string arrStr(T arr, int n) {
+	stringstream s; s << "[";
+	for (int i = 0; i < n - 1; i++) s << arr[i] << ",";
+	s << arr[n - 1] << "]";
+	return s.str();
+}
+template<typename t>
+void evars(vector<vector<t>> a){
+    for(auto i:a){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+#define EVARS(args...) {__evars_begin(__LINE__); __evars(split(#args, ',').begin(), args);}
+ 
+inline void __evars_begin(int line) { cout << "#" << line << ": "; }
+template<typename T> inline void __evars_out_var(vector<T> val) { cout << arrStr(val, val.size()); }
+template<typename T> inline void __evars_out_var(T* val) { cout << arrStr(val, 10); }
+template<typename T> inline void __evars_out_var(T val) { cout << val; }
+inline void __evars(vector<string>::iterator it) { cout << endl; }
+ 
+template<typename T, typename... Args>
+inline void __evars(vector<string>::iterator it, T a, Args... args) {
+	cout << it->substr((*it)[0] == ' ', it->length()) << "=";
+	__evars_out_var(a);
+	cout << "; ";
+	__evars(++it, args...);
+}
+ 
+struct Edge {
+    int a, b, cost;
+};
+
+int n, m, v;
+vector<Edge> edges;
+const int INF = 1000000000;
+
+void solve()
+{
+    vector<int> d(n, INF);
+    d[v] = 0;
+    vector<int> p(n, -1);
+    int x;
+    for (int i = 0; i < n; ++i) {
+        x = -1;
+        for (Edge e : edges)
+            if (d[e.a] < INF)
+                if (d[e.b] > d[e.a] + e.cost) {
+                    d[e.b] = max(-INF, d[e.a] + e.cost); // -INF to avoid int overflow
+                    p[e.b] = e.a;
+                    x = e.b;
+                }
+    }
+
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else {
+        int y = x;
+        for (int i = 0; i < n; ++i)
+            y = p[y];
+
+        vector<int> path;
+        for (int cur = y;; cur = p[cur]) {
+            path.push_back(cur);
+            if (cur == y && path.size() > 1)
+                break;
+        }
+        reverse(path.begin(), path.end());
+
+        cout << "Negative cycle: ";
+        for (int u : path)
+            cout << u << ' ';
+    }
+}
+
+int32_t main()
+{
+    clock_t begin = clock();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    yash;
+    w()
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
