345f7b9008d83a43c95d120ec5c36d0c5be43713
Yash Mohan <yashmohan.nitt@gmail.com>
2025-01-11 10:47:17 +0530

    Added DSU

---
+++ Graph/DSU/DSU template.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+ 
+#define endl            "\n"
+#define all(x)           x.begin(), x.end()
+#define yash            ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define w()             int t; cin>>t; while(t--)
+#define rep(i,a,b)      for(int i=a;i<b;++i)
+#define vpii            vector<pair<int, int>>
+#define vi              vector<int>
+#define vvi             vector<vector<int>>
+#define read(v,n)       rep(i,0,n) cin>>v[i];
+#define int             long long
+#define ull             unsigned long long
+#define out(v,n)        rep(i,0,n) {cout<<v[i]<<" ";}cout<<endl;
+#define pq              priority_queue
+#define mod             1000000007
+ 
+using namespace std;
+vector<string> split(const string& s, char c) {
+	vector<string> v; stringstream ss(s); string x;
+	while (getline(ss, x, c)) v.emplace_back(x); return move(v);
+}
+template<typename T, typename... Args>
+inline string arrStr(T arr, int n) {
+	stringstream s; s << "[";
+	for (int i = 0; i < n - 1; i++) s << arr[i] << ",";
+	s << arr[n - 1] << "]";
+	return s.str();
+}
+template<typename t>
+void evars(vector<vector<t>> a){
+    for(auto i:a){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+#define EVARS(args...) {__evars_begin(__LINE__); __evars(split(#args, ',').begin(), args);}
+ 
+inline void __evars_begin(int line) { cout << "#" << line << ": "; }
+template<typename T> inline void __evars_out_var(vector<T> val) { cout << arrStr(val, val.size()); }
+template<typename T> inline void __evars_out_var(T* val) { cout << arrStr(val, 10); }
+template<typename T> inline void __evars_out_var(T val) { cout << val; }
+inline void __evars(vector<string>::iterator it) { cout << endl; }
+ 
+template<typename T, typename... Args>
+inline void __evars(vector<string>::iterator it, T a, Args... args) {
+	cout << it->substr((*it)[0] == ' ', it->length()) << "=";
+	__evars_out_var(a);
+	cout << "; ";
+	__evars(++it, args...);
+}
+ 
+class DSU {
+private:
+    vector<int> parent;
+    vector<int> rank; // Alternatively, use size if preferred.
+
+public:
+    // Constructor to initialize DSU with n elements.
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 1); // Initial rank (or size) of each set is 1.
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i; // Each element is its own parent initially.
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // Path compression.
+        }
+        return parent[x];
+    }
+
+    // Union two sets by rank (or size).
+    bool union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) {
+            return false;
+        }
+
+        // Union by rank: attach the smaller tree under the larger tree.
+        if(rank[rootX] < rank[rootY]) {
+        	swap(rootX, rootY);
+        }
+        
+        parent[rootX] = rootY; // Merge the smaller tree to larger tree (based on rank)
+
+        if(rank[rootX] == rank[rootY]) {
+        	rank[rootX] += 1; // Increment rank only when both ranks are equal.
+        }
+        return true; // Union was successful.
+    }
+
+    // Check if two elements are in the same set.
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+void solve() {
+	
+}
+ 
+int32_t main()
+{
+    clock_t begin = clock();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    yash;
+    w()
+    solve();
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
